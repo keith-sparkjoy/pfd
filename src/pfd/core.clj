a33a0fc5f041904e3d3ebdbe0d5b912f6057f46b
Keith Sparkjoy <keith@Keiths-MacBook-Air.local>
2016-09-24 07:18:09 -0600

    Added tracking of most recent SID so we can determine success or failure once we finish a process.

--- src/pfd/core.clj
+++ src/pfd/core.clj
@@ -19,7 +19,7 @@
   [pfd]
   ((:steps pfd) (:beginning-sid pfd)))
 
-(defn add-vasteps
+(defn add-vasids
   "All value-added steps for a process can be found by traversing steps from the beginning through all directives and consequents. This function computes them ahead of time and adds them to the PFD."
   [{:keys [steps] :as pfd}]
   (letfn [(next-va-step [step]
@@ -36,7 +36,7 @@
   [pfd]
   (-> pfd
       map-steps
-      add-vasteps))
+      add-vasids))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
@@ -52,99 +52,15 @@
   [step]
   (contains? step :predicate))
 
-(defn current-step
-  [{:keys [pfd sid]}]
-  (get-in pfd [:steps sid]))
-
 (defn va-sid?
+  "Does this SID identify a value-added step?"
   [pfd sid]
   (contains? (:vasids pfd) sid))
 
-(defn text-for-current-step
-  [{:keys [pfd sid] :as cursor}]
-  (if (nil? sid)
-    (:eboundary pfd)
-    (let [step (current-step cursor)
-          text (or (:directive step)
-                   (:predicate step))]
-      (if (va-sid? pfd sid)
-        text
-        (str "(NVA) " text)))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; cursors
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defn new-cursor
-  "Create a cursor to walk through a process flow diagram"
-  [pfd]
-  {:pfd pfd
-   :sid (:beginning-sid pfd)})
-
-(defn nav-cursor
-  "Returns a function that will navigate the cursor to the next step"
-  [type-predicate get-next-sid]
-  (fn [cursor]
-    (let [step (current-step cursor)
-          new-cursor (when (type-predicate step)
-                       (let [next-sid (get-next-sid step)]
-                         (update-in cursor [:sid] (fn [old-sid] next-sid))))]
-      (or new-cursor cursor))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; REPL experimentation stuff
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defn make-sample-pfd
-  []
-  (let [pfd {:name "OpenFrontDoor"
-             :aim "Open front door"
-             :bboundary "You are standing outside the door."
-             :eboundary "The door is open and you are standing inside."
-             :steps [{:sid 5
-                      :directive "Turn the door knob."
-                      :next 3}
-                     {:sid 3
-                      :predicate "Is the door locked?"
-                      :not true
-                      :consequent 42
-                      :alternate 9}
-                     {:sid 42
-                      :directive "Gently push the door open and step inside."}
-                     {:sid 9
-                      :directive "Retrieve house key from pocket"
-                      :next 67}
-                     {:sid 67
-                      :directive "Insert key and twist counterclockwise."
-                      :next 5}]
-             :beginning-sid 5}]
-    (add-computed-fields pfd)))
-
-(def tpfd (atom (make-sample-pfd)))
-(def cursor (atom nil))
-
-(defn run []
-  (reset! cursor (new-cursor @tpfd))
-  (let [{:keys [bboundary]} @tpfd]
-    (str bboundary " " (text-for-current-step @cursor))))
-
-(defn ok []
-  (swap! cursor (nav-cursor is-directive? :next))
-  (text-for-current-step @cursor))
-
-(defn y []
-  (letfn [(get-next-sid
-            [{:keys [not consequent alternate]}]
-            (if not alternate consequent))]
-    (swap! cursor (nav-cursor is-predicate? get-next-sid))
-    (text-for-current-step @cursor)))
-
-(defn n []
-  (letfn [(get-next-sid
-            [{:keys [not consequent alternate]}]
-            (if not consequent alternate))]
-    (swap! cursor (nav-cursor is-predicate? get-next-sid))
-    (text-for-current-step @cursor)))
+(defn get-next-sid-for-predicate
+  "Given an answer, returns a function that will find the next step from a given predicate step"
+  [y?]
+  (fn [step]
+    ((if y? first second)
+     ((if (:not step) reverse identity)
+      (vals (select-keys step [:consequent :alternate]))))))
---
+++ src/pfd/repl_nav.clj
@@ -0,0 +1,113 @@
+(ns pfd.repl-nav
+  (:require [pfd.core :refer :all]))
+
+(defn make-sample-pfd
+  []
+  (let [pfd {:name "OpenFrontDoor"
+             :aim "Open front door"
+             :bboundary "You are standing outside the door."
+             :eboundary "The door is open and you are standing inside."
+             :steps [{:sid 5
+                      :directive "Turn the door knob."
+                      :next 3}
+                     {:sid 3
+                      :predicate "Is the door locked?"
+                      :not true
+                      :consequent 42
+                      :alternate 9}
+                     {:sid 42
+                      :directive "Gently push the door open and step inside."}
+                     {:sid 9
+                      :directive "Retrieve house key from pocket"
+                      :next 55}
+                     {:sid 55
+                      :predicate "Do you have the key?"
+                      :consequent 67}
+                     {:sid 67
+                      :directive "Insert key and twist counterclockwise."
+                      :next 5}]
+             :beginning-sid 5}]
+    (add-computed-fields pfd)))
+
+(def tpfd (atom (make-sample-pfd)))
+(def cursor (atom nil))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; REPL nav cursor
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn new-cursor
+  "Create a cursor to walk through a process flow diagram"
+  ([pfd]
+   {:pfd pfd
+    :sid (:beginning-sid pfd)})
+  ([pfd sid most-recent-sid]
+   {:pfd pfd
+    :sid sid
+    :mrsid most-recent-sid}))
+
+(defn current-step
+  [{:keys [pfd sid]}]
+  (get-in pfd [:steps sid]))
+
+(defn end?
+  [{:keys [sid]}]
+  (nil? sid))
+
+(defn ended-successfully?
+  [{:keys [pfd mrsid] :as cursor}]
+  (assert (end? cursor))
+  (va-sid? pfd mrsid))
+
+(defn nav-cursor
+  "Returns a function that will navigate the cursor to the next step"
+  [expected-type? get-next-sid]
+  (fn [cursor]
+    (let [step (current-step cursor)
+          new-cursor (when (expected-type? step)
+                       (let [most-recent-sid (:sid step)
+                             pfd (:pfd cursor)
+                             next-sid (get-next-sid step)]
+                         (new-cursor pfd next-sid most-recent-sid)))]
+      (or new-cursor cursor))))
+
+(defn text-for-current-step
+  [{:keys [pfd sid] :as cursor}]
+  (if (end? cursor)
+    (if (ended-successfully? cursor)
+      (str "Success! " (:eboundary pfd))
+      "Failure :-(")
+    (let [step (current-step cursor)
+          text (or (:directive step)
+                   (:predicate step))]
+      (if (va-sid? pfd sid)
+        text
+        (str "(NVA) " text)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; REPL nav commands
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn run []
+  (reset! cursor (new-cursor @tpfd))
+  (let [{:keys [bboundary]} @tpfd]
+    (str bboundary " " (text-for-current-step @cursor))))
+
+(defn ok []
+  (swap! cursor (nav-cursor is-directive? :next))
+  (text-for-current-step @cursor))
+
+(defn answer
+  [y?]
+  (swap! cursor (nav-cursor is-predicate? (get-next-sid-for-predicate y?)))
+  (text-for-current-step @cursor))
+
+(defn y []
+  (answer true))
+
+(defn n []
+  (answer false))
